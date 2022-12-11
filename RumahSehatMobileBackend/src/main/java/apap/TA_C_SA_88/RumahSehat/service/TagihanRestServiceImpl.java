8590f5ec712270f281d00667a704092f6863451c
Sulthan Muhammad Fasda <sulthan.muhammad01@ui.ac.id>
2022-12-11 21:35:23 +0700

    Feat7 appointment

--- RumahSehatMobileBackend/src/main/java/apap/TA_C_SA_88/RumahSehat/service/TagihanRestServiceImpl.java
+++ RumahSehatMobileBackend/src/main/java/apap/TA_C_SA_88/RumahSehat/service/TagihanRestServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +25,20 @@ public class TagihanRestServiceImpl implements TagihanRestService {
 	public List<TagihanModel> getAllTagihan() {
         return tagihanDb.findAll();
 	}
+
+    @Override
+    public TagihanModel getTagihanByKode(String kode) {
+        TagihanModel tagihan = tagihanDb.findByKode(kode);
+        return tagihan;
+    }
+
+    @Override
+    public TagihanModel updateStatusTagihan(TagihanModel newTagihan) {
+        TagihanModel tagihan = getTagihanByKode(newTagihan.getKode());
+        tagihan.setIsPaid(true);
+        tagihan.setTanggalBayar(LocalDateTime.now());
+        return tagihanDb.save(tagihan);
+    }
 
 	@Override
 	public List<TagihanModel> getUserTagihan(String username) {
