8c5612f4878a932b7f32613edb4524ef068e2d16
Fairuz Satria Mahardika <fairuz.satria@ui.ac.id>
2022-12-12 23:26:10 +0700

    Feat9,11 detail resep&create resep fix

---
+++ RumahSehat/src/main/java/apap/TA_C_SA_88/RumahSehat/service/TagihanService.java
@@ -0,0 +1,9 @@
+package apap.TA_C_SA_88.RumahSehat.service;
+
+import java.util.List;
+
+import apap.TA_C_SA_88.RumahSehat.model.TagihanModel;
+
+public interface TagihanService {
+    List<List<TagihanModel>> getTagihanAllRentangUmur();
+}
